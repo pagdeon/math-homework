1ef355a9f573fdd93c476c1dfc0b79227bc4458b
pagdeon <pag.deon@gmx.com>
2025-03-04 12:02:19 +0000

    Add 4.js

---
+++ src/4.js
@@ -0,0 +1,19 @@
+const mathHomework = {
+  "problems": [
+    {
+      "number": 1,
+      "text": "What is the value of x in the equation 2x + 5 = 11?",
+      "solution": "x = 4"
+    },
+    {
+      "number": 2,
+      "text": "Simplify the expression: (3x^2 + 5x - 2) / (x + 1)",
+      "solution": "(3x^2 + 5x - 2) / (x + 1)"
+    },
+    {
+      "number": 3,
+      "text": "Solve for x in the equation: 2x = 10",
+      "solution": "x = 5"
+    }
+  ]
+};
